1f5c99c60bd01742eb9f232e35a4c0ba12e0379d
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-11-25 22:08:57 +0300

    initial commit

---
+++ hood/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import *
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio','profile_pic']
+
+class HoodForm(forms.ModelForm):
+    class Meta:
+        model = Neighbourhood
+        exclude = ['admin']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['user', 'neighbourhood']
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+            model = Business
+            exclude = ['neighbourhood', 'profile']
+class NewCommentForm(forms.ModelForm):
+  class Meta:
+    model = Comment
+    exclude = ['post','postername','pub_date']
