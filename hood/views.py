e0a75e75c49c7d039aa0d417a094954e389fd765
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-11-26 12:33:33 +0300

    remove hmac

--- hood/views.py
+++ hood/views.py
@@ -23,22 +23,6 @@ def signup(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.is_active = False
-            user.save()
-            current_site = get_current_site(request)
-            mail_subject = 'Activate your HoodWatch account.'
-            message = render_to_string('acc_active_email.html', {
-                'user': user,
-                'domain': current_site.domain,
-                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
-                'token':account_activation_token.make_token(user),
-            })
-            to_email = form.cleaned_data.get('email')
-            email = EmailMessage(
-                        mail_subject, message, to=[to_email]
-            )
-            email.send()
-            return HttpResponse('Please confirm your email address to complete the registration')
     else:
         form = SignupForm()
     return render(request, 'signup.html', {'form': form})
@@ -169,7 +153,7 @@ def newcomment(request,id):
             comment.save()
     else:
         form = NewCommentForm()
-        
+
     return render(request, 'newcomment.html',{'brush':brush,"comments":comments,"form":form})
 
 
