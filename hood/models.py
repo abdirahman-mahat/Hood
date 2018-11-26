00c84af07134eb4208bcc43daf386f7321b57a4e
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-11-26 08:59:21 +0300

    add business model

--- hood/models.py
+++ hood/models.py
@@ -38,21 +38,13 @@ def create_user_profile(sender, instance, created, **kwargs):
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length = 300)
+    image = models.ImageField(upload_to='neighimage/', null=True)
+    admin = models.ForeignKey(Profile, related_name='hoods', null=True)
+    description = models.CharField(max_length = 300,default='My hood!!!')
 
 
-class Business(models.Model):
-    name = models.CharField(max_length=30)
-    category = models.CharField(max_length=30)
-    phone_number = models.CharField(max_length=10)
-    image = models.ImageField(upload_to='bsimage/')
-    description = models.CharField(max_length = 300)
-    neighbourhood = models.ForeignKey(Neighbourhood, related_name='businesses')
-    profile = models.ForeignKey(Profile, related_name='profiles')
-
-    @classmethod
-    def search_by_name(cls,search_term):
-        business = cls.objects.filter(title__icontains=search_term)
-        return business
 
 
 
