d42bbf19547faef8454937b54b00abbb0e701f11
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-11-26 08:58:56 +0300

    add neighbourhood model

--- hood/models.py
+++ hood/models.py
@@ -5,13 +5,40 @@ from django.dispatch import receiver
 from django.core.validators import MinValueValidator,MaxValueValidator
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    bio = models.CharField(max_length=100)
+    profile_pic = models.ImageField(upload_to='profile/')
+    pub_date_created = models.DateTimeField(auto_now_add=True, null=True)
+    neighbourhood = models.ForeignKey('Neighbourhood', blank=True, null=True)
+
+    def __str__(self):
+        return self.first_name
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def get_profiles(cls):
+        profiles = cls.objects.all()
+        return profiles
 
 
-class Neighbourhood(models.Model):
-    name = models.CharField(max_length = 300)
-    image = models.ImageField(upload_to='neighimage/', null=True)
-    admin = models.ForeignKey(Profile, related_name='hoods', null=True)
-    description = models.CharField(max_length = 300,default='My hood!!!')
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
 
 class Business(models.Model):
     name = models.CharField(max_length=30)
